0afa72df4b4401e1ab95805e1e68eada3eb07d1e
Bunogi <haakon.jordet@gmail.com>
2016-01-27 01:22:07 +0100

    Remove unused files

--- include/input.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ncurses.h>
-
-#include "world.hpp"
-
-namespace Input {
-	extern void handleInput(World::Player& player);
-}
--- include/screen.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace ScreenSizes {
-	extern int stdW;
-	extern int stdH;
-
-	extern int worldW;
-	extern int worldH;
-}
